cf8b5912b42b8f406fd0b3be304801e15bb38945
idko2004 <ivancito72011@gmail.com>
2024-06-17 11:43:24 -0400

    traducciones mdview

--- js/language.js
+++ js/language.js
@@ -18,7 +18,11 @@ const allTexts =
 	'tmatrix': {es: 'Dot Matrix: "Simulador" de una pantalla dot matrix', en: 'Dot Matrix: "Simulating" a dot matrix screen'},
 	'tconvertmusic': {es: 'convertMusic: Usa ffmpeg para convertir una carpeta de archivos en un formato a otro', en: 'convertMusic: Use ffmpeg to convert a folder of one type of files to another'},
 	'trandomc': {es: 'random-c: Genera un número aleatorio', en: 'random-c: Generate a random number'},
-	'albumcover': {es: 'Carátula del álbum recomendado', en: 'Cover of the recomended album'}
+	'albumcover': {es: 'Carátula del álbum recomendado', en: 'Cover of the recomended album'},
+	'mmdview1': {es: 'md-view es un visualizador de markdown para el navegador.', en: 'md-view is a markdown reader for the browser.'},
+	'mmdview2': {es: 'Lo hice porque no podía encontrar un lector de markdown que pudiera guardar pdfs, no fuese feo, dejase cambiar la fuente y no fuese difícil de instalar.', en: "I made it because i couldn't find a markdown reader that could save pdfs, wasn't ugly, let me change the font and wasn't a pain to install."},
+	'mmdview3': {es: 'Es un simple visualizador, no puede editar archivos, y seguramente ni sea del todo compatible con markdown, pero para archivos .md que sean simples, suele funcionar.', en: "It's just a simple reader, it can't edit files, and it isn't full compatible with markdown (may not even be markdown compliant, idk), but for simple .md files, it mostly works."},
+	'madewithhelp': {es: 'Hecho con la ayuda de:', en: 'Made with the help of:'}
 }
 
 function updateAllTexts()
