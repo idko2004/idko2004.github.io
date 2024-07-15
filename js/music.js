e2acd7890433318dcf110a78ada6ef7fd1dfe249
idko2004 <ivancito72011@gmail.com>
2024-07-15 13:25:07 -0400

    músicas en sus archivos aparte

--- js/music.js
+++ js/music.js
@@ -1,479 +1,81 @@
-const musics =
-[
+let albums;
+let artists;
+let musicFiles;
+let music;
+
+const amusictitle = document.getElementById('amusictitle');
+const listenlink = document.getElementById('listenlink');
+const amusicalbum = document.getElementById('amusicalbum');
+const amusicartist = document.getElementById('amusicartist');
+const divmusiccover = document.getElementById('divmusiccover');
+const musiclyric = document.getElementById('musiclyric');
+
+async function setMusicRecomendation()
+{
+	let musicInfoResponse;
+	let musicInfo;
+	try
 	{
-		title: 'Cabinet Man',
-		artist: 'Lemon Demon',
-		album: 'Spirit Phone',
-		url: 'https://music.youtube.com/watch?v=CdxtcvnnlXI',
-		lyrics:
-		[
-			"You can't win me, I can't be beat.",
-			"I won't hurt you unless you cheat.",
-			"You can't see me behind the screen.",
-			"I'm half human and half machine."
-		]
-	},
-	{
-		title: 'No Eyed Girl',
-		artist: 'Lemon Demon',
-		album: 'Spirit Phone',
-		url: 'https://music.youtube.com/watch?v=IqFBdBWK9bc',
-		lyrics:
-		[
-			"Oh, knowing what we know, knowing we don't know.",
-			"There's too much light, blinding white.",
-			"If I spoke your language I could tell you how I feel, but your language isn't real.",
-			"I might go insane if I learn your full name."
-		]
-	},
-	{
-		title: 'Ancient Aliens',
-		artist: 'Lemon Demon',
-		album: 'Spirit Phone',
-		url: 'https://music.youtube.com/watch?v=tKE1hC9mz7Y',
-		lyrics:
-		[
-			"You burn my eyes.",
-			"You speak in my mind.",
-			"I can't help you. You're not human.",
-			"Strange things happen. For no reason. That's all I know."
-		]
-	},
-	{
-		title: 'Soft Fuzzy Man',
-		artist: 'Lemon Demon',
-		album: 'Spirit Phone',
-		url: 'https://music.youtube.com/watch?v=7AQdzGdieB8',
-		lyrics:
-		[
-			"What you girls really need's a soft, fuzzy man.",
-			"So please, baby, please, baby step into the mist.",
-			"You don't understand, there's nothing quite like a soft, fuzzy man."
-		]
-	},
-	{
-		title: 'Spiral of Ants',
-		artist: 'Lemon Demon',
-		album: 'Spirit Phone',
-		url: 'https://music.youtube.com/watch?v=k0V9UqeSVgk',
-		lyrics:
-		[
-			"The Circle rules your life."
-		]
-	},
-	{
-		title: 'Angry People',
-		artist: 'Lemon Demon',
-		album: 'Spirit Phone',
-		url: 'https://music.youtube.com/watch?v=EehtkGu7Imk',
-		lyrics:
-		[
-			"There is anger in this country.",
-			"We're angry people!",
-			"We're completely normal!"
-		]
-	},
-	{
-		title: 'Aurora Borealis',
-		artist: 'Lemon Demon',
-		album: 'I Am Become Christmas',
-		url: 'https://music.youtube.com/watch?v=rbMY-NMT_vc',
-		lyrics:
-		[
-			"Is this the first time that you've ever seen. Aurora Borealis crush mankind?"
-		]
-	},
-	{
-		title: 'CryptoSanta',
-		artist: 'Lemon Demon',
-		album: 'I Am Become Christmas',
-		url: 'https://music.youtube.com/watch?v=KXJwiwjgyD4',
-		lyrics:
-		[
-			"New dimensions!. Now I'm presents!",
-			"New dimensions! Full of presents!"
-		]
-	},
-	{
-		title: 'CryptoSanta',
-		artist: 'Lemon Demon',
-		album: 'I Am Become Christmas',
-		url: 'https://music.youtube.com/watch?v=KXJwiwjgyD4',
-		lyrics:
-		[
-			"New dimensions!. Now I'm presents!",
-			"New dimensions! Full of presents!"
-		]
-	},
-	{
-		title: 'Amnesia Was Her Name',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=Onw61aL6dcU',
-		lyrics:
-		[
-			"The doctor said I had tomato loss.",
-			"We had spaghetti with long-term memory sauce."
-		]
-	},
-	{
-		title: "The Only House That's Not on Fire (Yet)",
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=LwntIpXvV-Q',
-		lyrics:
-		[
-			"The punchline is there is no punchline.",
-			"The punchline is that there is no punchline."
-		]
-	},
-	{
-		title: 'The Ocean',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=5P9_FHeFRcc',
-		lyrics:
-		[
-			"Now my body's on land but my heart will reside in the only place I've ever felt pacified, or atlanticized.",
-		]
-	},
-	{
-		title: 'The Afternoon',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=qbxJgfV0mEA',
-		lyrics:
-		[
-			"See the footprints in the cave? I would follow them, if I were brave to find out why there's no second set of footprints in the cave.",
-			"In the attic of this house. On the wall behind a propped up couch is a door that somehow leads into the basement of this house."
-		]
-	},
-	{
-		title: 'The Machine',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=849q2h1OkKs',
-		lyrics:
-		[
-			"That's the beauty of it. It doesn't do anything.",
-			"Lost in solipsism\nHe then slowly pulls a lever\nWhich sets off a mechanism\nWhich does nothing whatsoever\nBut the nothing that it does\nNegates the everything we know\nBecause it's screaming just because\nBecause it's neither friend or foe\nAnd so we label it a menace\nOr a grandiose work of art\nFrom its finale to its genesis\nWe slowly pull it all apart."
-		]
-	},
-	{
-		title: 'Bill Watterson',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=UTaszc1Q6eY',
-		lyrics:
-		[
-			"Don't you know I think you're the cat's meow?",
-			"Bill Watterson, can't you hear me?",
-			"Bill Watterson, please don't fear me.",
-			"Where's the tiger now? Where's the tiger now?"
-		]
-	},
-	{
-		title: 'Something Glowing',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=fIT1RSAcTDo',
-		lyrics:
-		[
-			"Indefinite and definitely dreamed about there's something.",
-			"There's something making noise.",
-			"Some terrible kaleidoskull\nThat ever so gently destroys\nThe minds of those who believe it.",
-			"Now there's a lightbulb at the bottom of the sea. It's glowing.",
-			"It's like sunlight, the upside-downness of it... It's saying something.",
-			"I can tell it's there by the way it's not quite there."
-		]
-	},
-	{
-		title: 'Sundial',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=tdbhU7EHAhc',
-		lyrics:
-		[
-			"Somehow something set my sundial backwards, tilted, and upside-down.",
-			"I think I think I am unsure of what to say.",
-			"Live long and prosper while the winter melts away.",
-			"I've got Anubis on my back and something in my shoe.",
-			"Don't walk backwards, don't be jealous.",
-			"I cease to see you when I start to shut my eyes."
-		]
-	},
-	{
-		title: 'Everyday French',
-		artist: 'Lemon Demon',
-		album: 'View-Monster',
-		url: 'https://music.youtube.com/watch?v=LEXhOSwe4pc',
-		lyrics:
-		[
-			"May I take a nap in your nose?",
-			"I have not showered in thirty-six days.",
-			"Monkeys are tickling my tummy.",
-			"Take those pork chops out of your brassiere.",
-			"Oh no, the babysitter exploded!",
-			"Let go of my grandmothers mustache!",
-			"Kiss me under the bologna tree.",
-			"Hey! Stop licking my kangaroo!"
-		]
-	},
-	{
-		title: 'Dead Sea Monkeys',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=nvs38PgCN68',
-		lyrics:
-		[
-			"I love my brine shrimp pals, they are my friends.\nI watch them float around inside their tank.",
-			"But when I came back home from school today.\nMy little friends were dead and I was sad.",
-			"The other day they were alive.\nAnd now they're floating at the top.\nI knew they wouldn't last forever.\nAnd now they're just a bunch of dead sea monkeys.",
-			"Will my sea monkeys go to heaven?",
-			"Will my sea monkeys go to hell?",
-			"I miss 'em, I miss 'em, me oh my!\nI miss 'em, I miss 'em, me oh my!\nI miss 'em, I miss 'em, me oh my!\nI miss 'em, I miss 'em, me oh my!"
-		]
-	},
-	{
-		title: 'Sky Is Not Blue',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=95tWFGej2RE',
-		lyrics:
-		[
-			"I made you these construction paper dolls.\nConnected by the hands with tiny little faces.\nAnd now I'll hang them on the walls.\nSo you can see the people filling up the spaces.",
-			"I think I am the only one to ever realize that I am.\nThe only one to realize what's true.\nWhen all the world is stuck in a jam:\nThe sky is not blue",
-			"Will my sea monkeys go to hell?",
-			"I might have stated what I thought before.\nI might have kept it in, my memory is failing.\nI might have stumbled through the door.\nYou might have stared and asked what I had been inhaling.",
-			"I'm setting up my tiny little town.\nA Lego set in which I'm planning to retire.\nIt's really time I settled down.\nBut not before I set everything on fire.",
-			"They tell me things I can't deny.\nThey tell me I'm a sap, I'm a square.\nThey tell me things I already knew.\nThe greatest of the secrets they share:\nThe sky is not blue."
-		]
-	},
-	{
-		title: 'Pumpkin Pie',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=0Npj_Evy54U',
-		lyrics:
-		[
-			"There lived a merchant in the days of old.\nWho is remembered for the things he sold.\nNo silly trinkets, silver, bronze, or gold.\nSo I'm told, so I'm told.",
-			"He made a most delicious pumpkin pie.\nWhich he presented to the passersby.\nBefore the sun set he'd be all but dry.\nFolks would buy the whole supply.",
-			"He lived alone, (He lived alone.).\nAnd every night he sat upon a throne of pumpkin pie.\nI do not know his name. (He'd never say his name.).\nHe never wanted all the fame that came with pumpkin pie.",
-			"Before you knew it he was in full swing.\nHis pumpkin pies became the next big thing.\nSoon he was called upon to feed the king.\nAnd they'd sing, feed the king."
-		]
-	},
-	{
-		title: 'When Robots Attack',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=lX-T7aUseUI',
-		lyrics:
-		[
-			"Lock up your children.\nLock up your pets as well (Your pets as well).\nIt's time to panic, because.\nSomething terrible is mad as hell!",
-			"They have malfunctioned, they went insane.\nHere come the buzzsaws and rattling chains.\nNo way to stop them, nowhere to run.\nRobotic destruction has now begun.",
-			"In man against machine, machine will conquer all."
-		]
-	},
-	{
-		title: 'Geeks in Love',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=_ZoEI-7j4xM',
-		lyrics:
-		[
-			"Nobody's cool as we.\nNor quite as esoterically far out.\nWe're so far out it's creepy",
-			"They have no roses.\nSubconsciously they envy us a lot",
-			"And when they hear our favorite bands, they wish that they were geeks in love.",
-			"As far as I can tell.\nWhile other lovers go through hell.\nWe'll know we're too cool for damnation.",
-			"When they see us on the street, they wish that they were geeks in love.",
-			"And when they hear us trick or treat, they wish that they were geeks in love.",
-			"Geekity-geekity-geeks in love."
-		]
-	},
-	{
-		title: 'New Way Out',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=zRtOVD3tIWY',
-		lyrics:
-		[
-			"Burger King fries on a dashboard underneath the burning hot sun.",
-			"Serpentine alarm clock, ringing up a horrid little kamikaze ding-ding-a-ding-ding-a-ding.",
-			"Nana korobi ya oki.\nRo-ma wa ichinichi ni shite narazu, baby",
-			"I don't know my way around.\nBut if I follow every sound.\nA new way out is soon surrounding me",
-			"And lo and behold, the very next morning.\nThey climbed out of bed and gazed out upon the morning world.\nAnd to their complete and utter amazement.\nIt had not snowed at all.\nThe end.",
-			"Don't mean to bug, but I thought I'd tell you that your shoes are untied.",
-			"Oh, running 'round in circles like a Chupacabra Capybara."
-		]
-	},
-	{
-		title: 'Kitten Is Angry',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=zjf2FKlZl9M',
-		lyrics:
-		[
-			"My kitten, my kitty cat.\nWhen she's content she purrs.\nShe thinks the house is hers.\nBut you won't like her when she's angry.\nNo, you won't like her when she's angry.\nAngry, angry.",
-			"Kitten is angry, kitten is offended.\nFur standing out and little claws extended.",
-			"Don't touch her, don't look at her.\nMy kitten knows tae kwon do.\nShe can take you out, I know.\nI've seen it before and it's not pretty.\nShe's really one ferocious kitty cat now.",
-			"She's deadly, she'll hurt you.\nShe'll show you endless pain.\nHer little whiskers are stained with the blood of those that she has slain.\nBelieve me, she will go insane.\nShe will go insane."
-		]
-	},
-	{
-		title: 'The Ceiling',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=Q7DSeyUX7yk',
-		lyrics:
-		[
-			"When did my room get a ceiling?\nI've never seen it before.",
-			'Sit down, you walking disaster.\nLook up and ask yourself.\nWhat can I see in the plaster?',
-			"I'd like to hop on a space ride.\nExit the universe.\nSee how it looks from the outside."
-		]
-	},
-	{
-		title: 'Rainwater',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=0ukn5UD8xm4',
-		lyrics:
-		[
-			"Is it safe to drink the rainwater?"
-		]
-	},
-	{
-		title: 'Dizziful Bliss',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=wen1_32bCSo',
-		lyrics:
-		[
-			"If you are not busy right now. Close your eyes and spin in your chair."
-		]
-	},
-	{
-		title: 'Word Disassociation',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=bil3TUilhsw',
-		lyrics:
-		[
-			"Enemy lasagna.",
-			"Robust below wax.",
-			"Semiautomatic aqua",
-			"Accompany slacks.",
-			"Why coffee gymnastic.",
-			"Motorcycle unibrow.",
-			"Existential plastic extra nightly cow.",
-			"Damn jettison goodbye through.",
-			"Everything center who.",
-			"Spidery concubine.",
-			"Pale lickity-split remorse.",
-			"Vitamin after force.",
-			"Already nested human wine.",
-			"Flight.",
-			"Luminary uprise.",
-			"Entanglement broke.",
-			"Unsophisticated clockwise.",
-			"Holiday way smoke.",
-			"Abundant various.",
-			"Metaphorically applause.",
-			"Underneath hilarious oxymoron claws.",
-			"Rectangular awkward hurt.",
-			"Million controvert",
-			"Never undressing sneer.",
-			"Blue therapy fall inside.",
-			"Father dethrone applied.",
-			"Guillotine apprehensive engineer.",
-			"Prance omelette stalking chimneysweep.",
-			"Eleven hatred earmuff okay rathskeller.",
-			"My elusive hula yellow sketching creamy helium gentlemanly.",
-			"communique.",
-			"Flouncy!",
-			"Panicky redundant.",
-			"Psychedelic while.",
-			"Raisin terrible abundant.",
-			"Polyurethane smile.",
-			"Scrumptious mechanical.",
-			"Jungle uncle wish.",
-			"Paleobotanical backwards licorice.",
-			"Truth medical entertain.",
-			"Cleverly porridge brain.",
-			"Jellyfish fingernail.",
-			"Agnostic oppressive wall.",
-			"Platypus parasol.",
-			"Sauntering sawdust opera monorail.",
-			"Letter no sly violin dust-bunny.",
-			"Explode serenade why spoil play drip.",
-			"Skullduggery freezer monocle pelican.",
-			"Cool milk freak tongue television staple-gun.",
-			"Mellow face bubblegum periscope fight silly.",
-			"Elephant akimbo paranoia sever maybe.",
-			"Crush toy spoon melt feather clear king weird.",
-			"Space love domino reality apostrophe.",
-			"Dollar jade velocity meringue assuming gentle mister.",
-			"Advertisement suitcase pining lobsters over murderous.",
-			"Distraction flames imposter a-capella crouch about bionic.",
-			"Ruby quickly antidisestablishmentarianism."
-		]
-	},
-	{
-		title: 'Scatterbrained',
-		artist: 'Lemon Demon',
-		album: 'Damn Skippy',
-		url: 'https://music.youtube.com/watch?v=rzjp3CaLEcU',
-		lyrics:
-		[
-			"Lately I've not been feeling so good.\nLike I'm just random knots in wood.\nI'm careful where I crawl.\nIf nothing else at all.",
-			"Some friends come by at seven 'o clock.\nThey ask if I'll go out for a walk.\nI tell them not today.\nBut they take me anyway.",
-			"Now won't you tell me why I feel so scatterbrained?\nThe moon still hasn't come back since it's waned.\nIt can't be what I'm looking for.\nI'm thinking but I don't know, oh, oh."
-		]
+		musicInfoResponse = await fetch("musicinfo.json");
+		musicInfo = await musicInfoResponse.json();
 	}
-]
+	catch(err)
+	{
+		alert(err);
+		return;
+	}
 
-let albums = {};
-let artist = {};
+	artists = musicInfo.artists;
+	albums = musicInfo.albums;
+	musicFiles = musicInfo['music-files'];
 
-const musicIndex = randomRange(0, musics.length - 1);
+	const musicIndex = randomRange(0, musicFiles.length - 1);
 
-//Title
-let element = document.getElementById('amusictitle');
+	let musicResponse;
+	try
+	{
+		musicResponse = await fetch(musicFiles[musicIndex]);
+		music = await musicResponse.json();
+	}
+	catch(err)
+	{
+		alert(err);
+		return;
+	}
 
-element.innerText = musics[musicIndex].title;
-element.href = musics[musicIndex].url;
-
-//Play button
-element = document.getElementById('listenlink');
-element.href = musics[musicIndex].url;
-
-//Album
-element = document.getElementById('amusicalbum');
-
-element.innerText = musics[musicIndex].album;
-element.href = albums[musics[musicIndex].album].url;
-
-//Artist
-element = document.getElementById('amusicartist');
-element.innerText = musics[musicIndex].artist;
-element.href = artist[musics[musicIndex].artist];
-
-//Cover
-element = document.getElementById('divmusiccover');
-element.style = `--music-cover:url("${albums[musics[musicIndex].album].cover}")`;
-element.addEventListener('click', ()=>
-{
-	document.getElementById('listenlink').click();
-});
-
-//Lyric
-let lyric;
-if(musics[musicIndex].lyrics.length == 1) lyric = 0;
-else if(musics[musicIndex].lyrics.length == 2)
-{
-	if(Math.random() > 0.49) lyric = 0;
-	else lyric = 1;
+	//Title
+	amusictitle.innerText = music.title;
+	amusictitle.href = music.url;
+	
+	//Play button
+	listenlink.href = music.url;
+	
+	//Album
+	amusicalbum.innerText = music.album;
+	amusicalbum.href = albums[music.album].url;
+	
+	//Artist
+	amusicartist.innerText = music.artist;
+	amusicartist.href = artists[music.artist];
+	
+	//Cover
+	divmusiccover.style = `--music-cover:url("${albums[music.album].cover}")`;
+	divmusiccover.addEventListener('click', ()=>
+	{
+		document.getElementById('listenlink').click();
+	});
+	
+	//Lyric
+	let lyric;
+	if(music.lyrics.length == 1) lyric = 0;
+	else if(music.lyrics.length == 2)
+	{
+		if(Math.random() > 0.49) lyric = 0;
+		else lyric = 1;
+	}
+	else lyric = randomRange(0, music.lyrics.length - 1);
+	
+	musiclyric.innerText = `"${music.lyrics[lyric]}"`;
 }
-else lyric = randomRange(0, musics[musicIndex].lyrics.length - 1);
 
-element = document.getElementById('musiclyric');
-element.innerText = `"${musics[musicIndex].lyrics[lyric]}"`;
+setMusicRecomendation();
