4681b9afc2c09fc74d4f5de69cd17692826e692a
idko2004 <ivancito72011@gmail.com>
2025-11-16 21:58:15 -0300

    cambiar un poquito como funciona el texto ondeante para que funcione en pantallas más pequeñas

--- js/tip-animation.js
+++ js/tip-animation.js
@@ -23,32 +23,42 @@ async function setupTipAnimation()
 		delayFormula += getText(textsToAnim[textToAnimIndex].textId).length;
 	}
 	delayFormula = velocity/delayFormula;
-
-
-	let elementsToAnimate = [];
 	let delay = 0;
 
 	for(let textToAnimIndex = 0; textToAnimIndex < textsToAnim.length; textToAnimIndex++)
 	{
 		let textToAnim = textsToAnim[textToAnimIndex];
 		let text = getText(textToAnim.textId);
-		for(let i = 0; i < text.length; i++)
+		let words = text.split(' ');
+		for(let wordIndex = 0; wordIndex < words.length; wordIndex++)
 		{
-			let element;
-			if(textToAnim.bold) element = document.createElement('b');
-			else element = document.createElement('span');
-
-			element.innerText = text[i];
-			
-			if(text[i] !== ' ')
+			let word = words[wordIndex];
+			let wordContainer = document.createElement('div');
+			wordContainer.classList.add('word-container');
+			for(let i = 0; i < word.length; i++)
 			{
+				let element;
+				if(textToAnim.bold) element = document.createElement('b');
+				else element = document.createElement('span');
+
+				element.innerText = word[i];
+				
 				element.style = `--dancing-text-delay: ${delay}ms; --velocity: ${velocity}ms`;
 				element.classList.add('appear-after-delay');
 				element.classList.add('dancing-text');
-				elementsToAnimate.push(element);
+				wordContainer.appendChild(element);
+				delay += delayFormula;
 			}
-			container.appendChild(element);
-			delay += delayFormula;
+
+			if(wordIndex < words.length - 1)
+			{
+				let element = document.createElement('span');
+				element.innerText = 'a';
+				element.classList.add('whitespace');
+				wordContainer.appendChild(element);
+			}
+
+			container.appendChild(wordContainer);
 		}
 	}
 /*
